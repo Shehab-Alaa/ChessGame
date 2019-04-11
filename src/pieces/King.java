e727488345abf6e02a9126a2d641f687ac89ca79
Shehab Alaa <39988066+ShehabElmasry@users.noreply.github.com>
2019-04-11 17:38:01 +0200

    Saad

--- src/pieces/King.java
+++ src/pieces/King.java
@@ -3,6 +3,7 @@ package pieces;
 import java.util.ArrayList;
 
 import extra.Position;
+import game.Board;
 import movements.BackwardMovement;
 import movements.DiagonalMovement;
 import movements.ForwardMovement;
@@ -11,15 +12,20 @@ import movements.RightSideMovement;
 
 public class King extends ChessPiece implements ForwardMovement , BackwardMovement , RightSideMovement , LeftSideMovement , DiagonalMovement{
 
+	private ArrayList<Position> ValidMoves;
+
 	public King(Position currentPosition, String color) {
 		super(currentPosition, color);
 	}
-	
-
 	@Override
 	public ArrayList<Position> getValidMoves() {
-	
-		return null;
+		ValidMoves=getValidBackwardMoves();
+		ValidMoves.addAll(getValidForwardMoves());
+		ValidMoves.addAll(getValidLeftMoves());
+		ValidMoves.addAll(getValidRightMoves());
+		ValidMoves.addAll(getValidDiagonalMoves());
+
+		return ValidMoves;
 	}
 
 	@Override
@@ -60,36 +66,60 @@ public class King extends ChessPiece implements ForwardMovement , BackwardMoveme
 
 	@Override
 	public ArrayList<Position> getValidDiagonalMoves() {
-		
-		return null;
+		ValidMoves=new ArrayList<Position>();
+
+		fillList(1, 1);
+		fillList(-1, -1);
+		fillList(-1, 1);
+		fillList(1, -1);		
+
+		return ValidMoves;
 	}
 
 
 	@Override
 	public ArrayList<Position> getValidLeftMoves() {
+		ValidMoves=new ArrayList<Position>();
 		
-		return null;
+		fillList(0, -1);
+		
+		return ValidMoves;
 	}
 
 
 	@Override
-	public ArrayList<Position> getValidRightMoves() {
+	public ArrayList<Position> getValidRightMoves() {		
+        ValidMoves=new ArrayList<Position>();
 		
-		return null;
+		fillList(0, 1);
+
+		return ValidMoves;
 	}
 
 
 	@Override
 	public ArrayList<Position> getValidBackwardMoves() {
+        ValidMoves=new ArrayList<Position>();
 		
-		return null;
+		fillList(1, 0);
+
+		return ValidMoves;
 	}
 
 
 	@Override
 	public ArrayList<Position> getValidForwardMoves() {
+        ValidMoves=new ArrayList<Position>();
 		
-		return null;
+		fillList(-1, 0);
+	
+		return ValidMoves;
 	}
-
+	private boolean check(int i,int j) {return i>=0&&i<8&&j>=0&&j<8;}
+	
+	private void  fillList(int i,int j) {
+		if(check(currentPosition.getRow()+i,currentPosition.getColumn()+j))
+			ValidMoves.add(new Position(currentPosition.getRow()+i, currentPosition.getColumn()+j));
+	}
+	
 }
